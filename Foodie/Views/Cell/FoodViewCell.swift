f698c150569aa1df5c6f0a2e44e3230b07ef96ca
Marwan Hisham <marwanhiisham@outlook.com>
2023-10-19 15:48:06 +0300

    Homescreen done

--- Foodie/Views/Cell/FoodViewCell.swift
+++ Foodie/Views/Cell/FoodViewCell.swift
@@ -11,7 +11,10 @@ struct FoodViewCell: View {
     let food: Food
     var body: some View {
         HStack{
-            Image("Image").resizable().aspectRatio(contentMode: .fit).clipShape(.buttonBorder).frame(width: 120, height: 90)
+            FoodRemoteImage(urlString: food.imageURL)
+                .aspectRatio(contentMode: .fit)
+                .clipShape(.buttonBorder)
+                .frame(width: 120, height: 90)
             VStack (alignment: .leading, spacing: 5) {
                 Text(food.name).font(.title2).fontWeight(.medium)
                 Text("$\(food.price, specifier: "%.2f")").font(.body).foregroundStyle(.secondary).fontWeight(.semibold)
